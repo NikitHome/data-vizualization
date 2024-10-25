49c8aa6ba651fc7bb20f79d480a81cd733e087bf
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-25 17:46:26 +0300

    Update rw_visual.py

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -1,27 +1,20 @@
 import matplotlib.pyplot as plt
+
 from random_walk import RandomWalk
 
-while True:
-    rw = RandomWalk(50_000)
-    rw.fill_walk()
+# Построение случайного блуждания
+rw = RandomWalk(50_000)
+rw.fill_walk()
 
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
+# Нанесение точек на диаграмму.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+point_numbers = range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
+           edgecolors='none', s=1)
 
-    point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
-               cmap=plt.cm.Blues, edgecolors='none', s=1)
+# Выделение первой и последней точек.
+ax.scatter(0, 0, c='green', edgecolors='none', s=50)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
 
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
-               edgecolors='none', s=100)
-
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    plt.show()
-
-    keep_running = input("Make another walk? (y/n): ")
-    if keep_running == 'n':
-        break
-
+plt.show()
