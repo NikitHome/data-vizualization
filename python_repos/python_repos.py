b692b95688b8a3eb12dff92971ccef363d5775f7
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-30 11:21:55 +0300

    Create python_repos.py

---
+++ python_repos/python_repos.py
@@ -0,0 +1,23 @@
+import requests
+
+# Создание вызова API и сохранение ответа.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Сохранение ответа API в переменной.
+response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Анализ информации о репозиториях.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
