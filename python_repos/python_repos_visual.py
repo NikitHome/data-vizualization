f3dbce536d8abae0587fe627f70815970eccbb2b
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-30 11:21:57 +0300

    Create python_repos_visual.py

---
+++ python_repos/python_repos_visual.py
@@ -0,0 +1,60 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Создание вызова API и сохранение ответа.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Обработчка результатов.
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_links, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    stars.append(repo_dict['stargazers_count'])
+    
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+    
+# Построение визуализации.
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {
+            'width': 1.5,
+            'color': 'rgb(25, 25, 25)'
+        },
+    },
+    'opacity': 0.6,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on Github',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},    
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},    
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
